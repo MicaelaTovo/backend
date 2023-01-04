4cd9017125528c17a842941f0d35b9db6fd4a9f7
micatovo <micaelatovo@gmail.com>
2023-01-03 21:34:54 -0300

    probando

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require('express')
-const ProductManager = require('./Backend/ProductManager')
 const app = express()
 
-// const productManager = new ProductManager('/Backend/products.json')
 
 app.get("/", function (req, res) {
-  const nombre = req.params.nombre;
-  const respuesta = `
-    <html>
-      <body> 
-        <h1 style= "color: red"> Bienvenido ${nombre}! </h1>
-      </body>
-    </html>
-  `;
-  res.send(respuesta)
+  res.send('Funcionando!')
 });
 
-app.get('/', function (req, res) {
-res.send('Hola gente')
-})
 
 
 app.listen(3000)

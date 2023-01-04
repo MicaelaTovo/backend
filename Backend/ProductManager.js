fd0859616e3a19314163f377987acb6cbf7549a4
micatovo <micaelatovo@gmail.com>
2023-01-04 00:13:08 -0300

    tercer trabajo

--- Backend/ProductManager.js
+++ Backend/ProductManager.js
@@ -1,90 +1,91 @@
 const fs = require('fs');
 
 class ProductManager {
-    constructor(path) {
-        this.path = path;
-        this.products = this.readFile();
+    constructor(filepath) {
+        this.filepath = filepath;
+        // this.products = this.readFile();
     }
 
-    readFile() {
-        const data = JSON.parse(fs.readFileSync(`./${this.path}`, 'utf8'));
-        return data;
-    }
+    // readFile() {
+    //     const data = JSON.parse(fs.readFileSync(this.filepath, 'utf8'));
+    //     return data;
+    // }
 
-    writeData(data){
-        let dataString = JSON.stringify(data);
-        fs.writeFileSync(`./${this.path}`, dataString);
-        return dataString;
-    }
+    // writeData(data){
+    //     let dataString = JSON.stringify(data);
+    //     fs.writeFileSync(`./${this.filepath}`, dataString);
+    //     return dataString;
+    // }
 
-    idGenerator() {
-        if (this.products.length > 0) {
-            let productsId = this.products.map(product => product.id);
-            return Math.max(...productsId) + 1;
-    } else {
-        let id = 1
-        return id;
-        }
-    }
+    // idGenerator() {
+    //     if (this.products.length > 0) {
+    //         let productsId = this.products.map(product => product.id);
+    //         return Math.max(...productsId) + 1;
+    // } else {
+    //     let id = 1
+    //     return id;
+    //     }
+    // }
     
     getProducts() {
-        let data = this.readFile();
-        console.log(data);
-        return data;
-    }
-    
-    addProduct(product){
-        if (this.products.find((p) => p.code == product.code)){
-            return console.log(`El producto con el codigo ${product.code} ya existe`)
-        } else if (!!!product.title || !!!product.description || !!!product.price || !!!product.thumbnail || !!!product.code || !!! product.stock) {
-            return console.log("Algun dato es nulo");
-        } else {
-            let data = this.readFile();
-            product.id = this.idGenerator();
-            data.push(product);
-            this.writeData(data);
-        }
-    }
-
-    getProductById(){
-        let data = this.readFile();
-        if (data.find(product => product.id === id)){ 
-            return console.log(`El producto con el id es ${product.id}`)
-        } else{ return console.log(`Not found`)
-            } 
-    }
-    
-    deleteProduct(id){
-        let data = this.readFile();
-        if(data.find(product => product.id === id)){
-            let products = data.filter(product => product.id !== id)
-            this.writeData(products)
-            return products;
-        } else { return this.console.log(`Not found`)
-            }
-    }
-
-    updateProduct(id, product) {
-        let data = this.readFile();
-        if(data.find((p) => p.id === product.id)){
-            let productDelete = data.filter(product => product.id !== id)
-            product.id = id;
-            productDelete.push(product);
-            this.writeData(productDelete);
-            return productDelete;
-        } else { return console.log(`Not found`)
-        }
+        const product = fs.readFileSync(this.filepath, 'utf8');;
+        return JSON.parse(product);
     }
 }
+//     addProduct(product){
+//         if (this.products.find((p) => p.code == product.code)){
+//             return console.log(`El producto con el codigo ${product.code} ya existe`)
+//         } else if (!!!product.title || !!!product.description || !!!product.price || !!!product.thumbnail || !!!product.code || !!! product.stock) {
+//             return console.log("Algun dato es nulo");
+//         } else {
+//             let data = this.readFile();
+//             product.id = this.idGenerator();
+//             data.push(product);
+//             this.writeData(data);
+//         }
+//     }
+
+//     getProductById(){
+//         let data = this.readFile();
+//         if (data.find(product => product.id === id)){ 
+//             return console.log(`El producto con el id es ${product.id}`)
+//         } else{ return console.log(`Not found`)
+//             } 
+//     }
+    
+//     deleteProduct(id){
+//         let data = this.readFile();
+//         if(data.find(product => product.id === id)){
+//             let products = data.filter(product => product.id !== id)
+//             this.writeData(products)
+//             return products;
+//         } else { return this.console.log(`Not found`)
+//             }
+//     }
+
+//     updateProduct(id, product) {
+//         let data = this.readFile();
+//         if(data.find((p) => p.id === product.id)){
+//             let productDelete = data.filter(product => product.id !== id)
+//             product.id = id;
+//             productDelete.push(product);
+//             this.writeData(productDelete);
+//             return productDelete;
+//         } else { return console.log(`Not found`)
+//         }
+//     }
+// }
 
 
-    const productManager = new ProductManager('Backend/products.json')
+    // const productManager = new ProductManager('./Backend/products.json')
+    // console.log(productManager.getProducts());
 
 
-    productManager.addProduct()
-    productManager.getProductById()
-    productManager.deleteProduct()
-    productManager.updateProduct()
+//     productManager.addProduct()
+//     productManager.getProductById()
+//     productManager.deleteProduct()
+//     productManager.updateProduct()
 
 
     module.exports = ProductManager;
+
--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const ProductManager = require('./Backend/ProductManager')
 const app = express()
+const PORT = 8080;
 
+const productManager = new ProductManager('./Backend/products.json')
 
-app.get("/", function (req, res) {
-  res.send('Funcionando!')
+app.get("/products", async (req, res) => {
+  const products = await productManager.getProducts();
+  const { limit } = req.query;
+
+  if (limit) return res.json(products.slice(0, limit))
+  else return res.json(products);
 });
 
+app.get("/products/:pid", async (req, res) => {
+  const products = await productManager.getProducts();
+  const { pid } = req.params;
+  const product = products.find(p => p.id === pid);
 
+  if (product) return res.status(200).json(product);
+  else return res.status(404).json({ message: "Product not found" });
 
-app.listen(3000)
+});
+
+app.listen(PORT, ()=>{
+  console.log(`listening on port ${PORT}`);
+})
